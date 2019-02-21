5af06f1519a9f9b80ac8821afc5708872c8efac4
Pedro de Almeida Santos <pedroalmeidasantos.00@gmail.com>
2019-02-21 16:26:12 -0300

    Projeto Web - Sem conteudo

---
+++ roadRunner/roadRunner/index.php
@@ -0,0 +1,170 @@
+<! DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>
+            Road Runner Cross Bikes SA
+        </title>
+        <link rel="stylesheet" type="text/css"  href="css/style.css">
+        <script src="slider/js/jquery-1.11.3.min.js" type="text/javascript"></script>
+        <script src="slider/js/jssor.slider-27.5.0.min.js" type="text/javascript"></script>
+        <script src="slider/js/ajuste.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <header>
+            <div id="caixa_header" class="center">
+                <div id="caixa_logo">
+                    <img src="logo/bike.png">
+                </div>
+
+                <div id="caixa_assuntos">
+                    <nav id="menu">
+                        <ul>
+                            <li>
+                                Notícias em Destaque
+                            </li>
+
+                            <li>
+                                Sobre a Loja
+                            </li>
+
+                            <li>
+                                Promoções
+                            </li>
+
+                            <li>
+                                Nossas Lojas
+                            </li>
+
+                            <li>
+                                Nossos Eventos
+                            </li>
+
+                            <li>
+                                Fale Conosco
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div id="caixa_login">
+                    <form name="frmlogin" method="" action="">
+                        <div class="caixa_login">
+                            <label class="user">Usuário</label>
+                            <input type="text" name="txt_usuario" class="text" value="">
+                        </div>
+                        <div class="caixa_login">
+                            <label class="senha">Senha</label>
+                            <input type="password" name="txt_senha" class="text" value="">
+                        </div>
+                        <div class="btn_login">
+                            <input type="submit" value="ENTRAR" class="btn">
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
+        </header>
+        <div id="ajust_slide">
+            
+        </div>
+        <!-- Slider -->
+        <div id="jssor_1" style="position:relative;margin:0 auto;top:0px;left:0px;width:1230px;height:380px;overflow:hidden;visibility:hidden;">
+        <!-- Loading Screen -->
+        <div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
+            <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="slider/img/spin.svg"/>
+        </div>
+        <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:1400px;height:380px;overflow:hidden;">
+            <div>
+                <img data-u="image" src="slider/img/001.jpg" />
+                <div data-u="thumb">Slide Description 001</div>
+            </div>
+            <div>
+                <img data-u="image" src="slider/img/002.jpg" />
+                <div data-u="thumb">Slide Description 002</div>
+            </div>
+            <div>
+                <img data-u="image" src="slider/img/003.jpg" />
+                <div data-u="thumb">Slide Description 003</div>
+            </div>
+            <div>
+                <img data-u="image" src="slider/img/004.jpg" />
+                <div data-u="thumb">Slide Description 004</div>
+            </div>
+            <div>
+                <img data-u="image" src="slider/img/005.jpg" />
+                <div data-u="thumb">Slide Description 005</div>
+            </div>
+            <div>
+                <img data-u="image" src="slider/img/006.jpg" />
+                <div data-u="thumb">Slide Description 006</div>
+            </div>
+            <div>
+                <img data-u="image" src="slider/img/007.jpg" />
+                <div data-u="thumb">Slide Description 007</div>
+            </div>
+            <div>
+                <img data-u="image" src="slider/img/008.jpg" />
+                <div data-u="thumb">Slide Description 008</div>
+            </div>
+            <div>
+                <img data-u="image" src="slider/img/009.jpg"/>
+                <div data-u="thumb">Slide Description 009</div>
+            </div>
+            <div>
+                <img data-u="image" src="slider/img/010.jpg" />
+                <div data-u="thumb">Slide Description 010</div>
+            </div>
+        </div>
+        <!-- Thumbnail Navigator -->
+        <div data-u="thumbnavigator" style="position:absolute;bottom:0px;left:0px;width:1230px;height:50px;color:#FFF;overflow:hidden;cursor:default;background-color:rgba(0,0,0,.5);">
+            <div data-u="slides">
+                <div data-u="prototype" style="position:absolute;top:0;left:0;width:1200px;height:50px;">
+                    <div data-u="thumbnailtemplate" style="position:absolute;top:0;left:0;width:100%;height:100%;font-family:arial,helvetica,verdana;font-weight:normal;line-height:50px;font-size:16px;padding-left:10px;box-sizing:border-box;"></div>
+                </div>
+            </div>
+        </div>
+        <!-- Setas de Navegação -->
+        <div data-u="arrowleft" class="jssora061" style="width:55px;height:55px;top:0px;left:25px;" data-autocenter="2" data-scale="0.75" data-scale-left="0.75">
+            <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
+                <path class="a" d="M11949,1919L5964.9,7771.7c-127.9,125.5-127.9,329.1,0,454.9L11949,14079"></path>
+            </svg>
+        </div>
+        <div data-u="arrowright" class="jssora061" style="width:55px;height:55px;top:0px;right:25px;" data-autocenter="2" data-scale="0.75" data-scale-right="0.75">
+            <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
+                <path class="a" d="M5869,1919l5984.1,5852.7c127.9,125.5,127.9,329.1,0,454.9L5869,14079"></path>
+            </svg>
+        </div>
+    </div>
+        <!-- Fim Slider -->
+        
+        <!-- CONTEUDO E MENU LATERAL -->
+    <section id="menu_conteudo" class="center">
+        <div id="menu_lateral">
+            <ul id="menu_lateral">
+                <li> 
+                    Home
+                </li>
+
+                <li> 
+                    Sobre 
+                </li>
+
+                <li> 
+                    Produtos 
+                </li>
+
+                <li> 
+                    Imagens 
+                </li>
+
+                <li> 
+                    Contatos 
+                </li>
+            </ul>
+        </div>
+    </section>
+    <footer class="center">
+        
+    </footer>
+    </body>
+</html>
